1392a39be23f5a10467df962839f6cb44ffe78f1
JumAK <JumAK@simplon.co>
2013-10-23 19:27:59 +0200

    Refactor: def class Todo

--- app.rb
+++ app.rb
@@ -1,36 +1,48 @@
-require "sinatra"
+ require "sinatra"
 
-def get_data
-	@tasks = []
-	IO.foreach('public/data.txt') do |line|
-		@tasks << line
+
+class Todo
+	def initialize
+		@lines = []
+		@path = 'public/data.txt'
+	end
+
+	def read
+		IO.foreach(@path) do |line|
+			@lines << line
+		end
+		return @lines
+	end
+	
+	def save
+		open(@path,'w') do |add|
+			add.puts @lines
+		end
 	end
 end
 
 get "/" do
 	#display the list of tasks written in data.txt
-	get_data
+	@liste = Todo.new
 	erb :index
-end
-
-get "/:id" do
-	#display the content of task.id if it exist in data.txt
 end
 
 post "/" do
 	#add task.id to data.txt
-	get_data
-	@tasks << params[:new]
-	open('public/data.txt','w') do |add|
-		add.puts @tasks
-	end
+	@liste = Todo.new
+	@liste.read << params[:new]
+	@liste.save
 	redirect "/"
 end
 
-put "/edit/:id" do
-	#replace the content of task.id in data.txt
-end
+# get "/:id" do
+# 	#display the content of task.id if it exist in data.txt
+# end
 
-delete "/delete/:id" do
-	#delete task.id in data.txt
-end
+# put "/edit/:id" do
+# 	#replace the content of task.id in data.txt
+# end
+
+# delete "/delete/:id" do
+# 	#delete task.id in data.txt
+# end
