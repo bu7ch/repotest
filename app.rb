7c6505cdaed56eb76c266329542844902622dbea
em-ak <emilien.ak@gmail.com>
2013-10-24 21:46:18 +0200

    Add delete suggestions

--- app.rb
+++ app.rb
@@ -3,42 +3,48 @@ require "sinatra/activerecord"
 
 set :database, "sqlite3:///foo.sqlite3"
 
+############################################## MODEL
+# Define our Task model
 class Task < ActiveRecord::Base
 	validates_presence_of :content
 end
 
-
+########################################## CONTROLER
 get "/" do
-	#display the list of tasks written in data.txt
+	# Display all Tasks
 	@tasks = Task.all
 	erb :index
 end
 
 post "/new" do
-	#add task.id to data.txt
+	# Creat a new Task
 	@task = Task.new(content: params[:fock])
 	@task.save
 	redirect "/"
 end
 
 get "/edit/:id" do
-	#replace the content of task.id
+	# Display Task.content in a form field
 	@task = Task.find(params[:id])
 	erb :edit
 end
 
 put "/edit/:id" do
+	# Update Task.content
 	@task = Task.find(params[:id])   
 	@task.update_attributes(content: params[:fock])
 	redirect "/"
 end
 
-# delete "todo/delete/:id" do
-# 	#delete task.id in data.txt
-# end
+delete "/delete/:id" do
+	# Delete a Task entry
+
+	# Task.delete or Task.destroy ???
+	# http://www.nickpeters.net/2007/12/21/delete-vs-destroy/
+end
 
 # get "/:id" do
-#  	#display the content of task.id if it exist in data.txt
+#  	# Display the content of task.id if it exist in the db
 #  	@tasks = Task.find(params[:id])
 #  	erb :index
 # end
