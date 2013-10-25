f50ac3505bd0221f275c6f21cecaef00b8e003b3
em-ak <emilien.ak@gmail.com>
2013-10-25 16:50:46 +0200

    Handle 404 errors + small refactoring

--- app.rb
+++ app.rb
@@ -4,12 +4,19 @@ require "sinatra/activerecord"
 set :database, "sqlite3:///foo.sqlite3"
 
 ############################################## MODEL
+
 # Define our Task model
 class Task < ActiveRecord::Base
 	validates_presence_of :content
 end
 
 ########################################## CONTROLER
+
+not_found do
+	# Handle 404 errors
+	"<h1>You're lost in the 404 area!</h1><br><a href='/'>Get back on track</a>"
+end
+
 get "/" do
 	# Display all Tasks
 	@tasks = Task.all
@@ -18,33 +25,31 @@ end
 
 post "/new" do
 	# Creat a new Task
-	@task = Task.new(content: params[:fock])
+	@task = Task.new content: params[:fock]
 	@task.save
 	redirect "/"
 end
 
 get "/edit/:id" do
 	# Display Task.content in a form field
-	@task = Task.find(params[:id])
+	@task = Task.find params[:id]
 	erb :edit
 end
 
 put "/edit/:id" do
 	# Update Task.content
-	@task = Task.find(params[:id])   
-	@task.update_attributes(content: params[:fock])
+	@task = Task.find params[:id]
+	@task.update_attributes content: params[:fock]
 	redirect "/"
 end
 
 delete "/delete/:id" do
-	# Delete a Task entry
+	# Delete the Task entry
 
 	# Task.delete or Task.destroy ???
 	# http://www.nickpeters.net/2007/12/21/delete-vs-destroy/
 end
 
-# get "/:id" do
-#  	# Display the content of task.id if it exist in the db
-#  	@tasks = Task.find(params[:id])
-#  	erb :index
-# end
+# Todo :
+# => Quand je clique sur le bouton 'trash', supprimer la tache
+# => 
